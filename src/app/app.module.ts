ec6928c80bf9ee5a5a4c8acbceff02e5fc1b84d4
sharopcha <napster1202@gmail.com>
2022-03-06 20:25:43 +0200

    setup material and header module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { HeaderModule } from './header/header.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    HeaderModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
