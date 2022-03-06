baa82b2326101703e7bb7efadf6dc2c32fe46810
sharopcha <napster1202@gmail.com>
2022-03-06 20:53:40 +0200

    auth component added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  @Output()
+  onSubmitEvent = new EventEmitter<any>();
 
-  constructor() { }
+  @Input()
+  submitLabel!: string;
 
-  ngOnInit(): void {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  password = new FormControl('', [Validators.required]);
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return '';
+  }
+
+  onSubmit() {
+    this.onSubmitEvent.emit({
+      email: this.email.value,
+      password: this.password.value,
+    });
+  }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AuthComponent
-  ],
+  declarations: [AuthComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
+  exports: [AuthComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
